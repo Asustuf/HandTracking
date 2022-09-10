4136e0ecbf847f3fa10ee6cfee7553ef4db6bcdc
Asustuf <88196094+Asustuf@users.noreply.github.com>
2022-09-10 20:18:56 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,167 @@
+import sys
+import cv2
+import mediapipe as mp
+import time
+from flask import Flask, render_template
+from math import hypot
+from ctypes import cast, POINTER
+from comtypes import CLSCTX_ALL
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
+import numpy as np
+
+from math import hypot
+import screen_brightness_control as sbc
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/index1')
+def index1():
+    return render_template('index.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+# function for handtracking model
+@app.route("/hand")
+def hand():
+    cap = cv2.VideoCapture(0)
+
+    mpHands = mp.solutions.hands
+    hands = mpHands.Hands()
+    mpDraw = mp.solutions.drawing_utils
+
+    pTime = 0
+    cTime = 0
+
+    while True:
+        success, img = cap.read()
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        results = hands.process(imgRGB)
+        # print(results.multi_hand_landmarks)
+
+        if results.multi_hand_landmarks:
+            for handlms in results.multi_hand_landmarks:
+                for id, lm in enumerate(handlms.landmark):
+                    h, w, c = img.shape
+                    cx, cy = int(lm.x * w), int(lm.y * h)
+                    print(id, cx, cy)
+                    cv2.circle(img, (cx, cy), 8, (255, 0, 255), cv2.FILLED)
+
+                mpDraw.draw_landmarks(img, handlms, mpHands.HAND_CONNECTIONS)
+
+        cTime = time.time()
+        fps = 1 / (cTime - pTime)
+        pTime = cTime
+
+        cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (233, 3, 233), 3)
+
+        cv2.imshow("Image", img)
+        cv2.waitKey(1)
+
+
+# function for volume
+@app.route("/volume")
+def volume():
+    cap = cv2.VideoCapture(0)
+
+    mpHands = mp.solutions.hands
+    hands = mpHands.Hands()
+    mpDraw = mp.solutions.drawing_utils
+
+    devices = AudioUtilities.GetSpeakers()
+    interface = devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+    volume = cast(interface, POINTER(IAudioEndpointVolume))
+
+    volMin, volMax = volume.GetVolumeRange()[:2]
+
+    while True:
+        success, img = cap.read()
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        results = hands.process(imgRGB)
+
+        lmList = []
+        if results.multi_hand_landmarks:
+            for handlandmark in results.multi_hand_landmarks:
+                for id, lm in enumerate(handlandmark.landmark):
+                    h, w, _ = img.shape
+                    cx, cy = int(lm.x * w), int(lm.y * h)
+                    lmList.append([id, cx, cy])
+                mpDraw.draw_landmarks(img, handlandmark, mpHands.HAND_CONNECTIONS)
+
+        if lmList != []:
+            x1, y1 = lmList[4][1], lmList[4][2]
+            x2, y2 = lmList[8][1], lmList[8][2]
+
+            cv2.circle(img, (x1, y1), 4, (255, 0, 0), cv2.FILLED)
+            cv2.circle(img, (x2, y2), 4, (255, 0, 0), cv2.FILLED)
+            cv2.line(img, (x1, y1), (x2, y2), (255, 0, 0), 3)
+
+            length = hypot(x2 - x1, y2 - y1)
+
+            vol = np.interp(length, [15, 220], [volMin, volMax])
+            print(vol, length)
+            volume.SetMasterVolumeLevel(vol, None)
+
+            # Hand range 15 - 220
+            # Volume range -63.5 - 0.0
+
+        cv2.imshow('Image', img)
+        if cv2.waitKey(1) & 0xff == ord('q'):
+            break
+
+@app.route("/brightness")
+def brightness():
+    cap = cv2.VideoCapture(0)
+
+    mpHands = mp.solutions.hands
+    hands = mpHands.Hands()
+    mpDraw = mp.solutions.drawing_utils
+
+    while True:
+        success, img = cap.read()
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        results = hands.process(imgRGB)
+
+        lmList = []
+        if results.multi_hand_landmarks:
+            for handlandmark in results.multi_hand_landmarks:
+                for id, lm in enumerate(handlandmark.landmark):
+                    h, w, _ = img.shape
+                    cx, cy = int(lm.x * w), int(lm.y * h)
+                    lmList.append([id, cx, cy])
+                mpDraw.draw_landmarks(img, handlandmark, mpHands.HAND_CONNECTIONS)
+
+        if lmList != []:
+            x1, y1 = lmList[4][1], lmList[4][2]
+            x2, y2 = lmList[8][1], lmList[8][2]
+
+            cv2.circle(img, (x1, y1), 4, (255, 0, 0), cv2.FILLED)
+            cv2.circle(img, (x2, y2), 4, (255, 0, 0), cv2.FILLED)
+            cv2.line(img, (x1, y1), (x2, y2), (255, 0, 0), 3)
+
+            length = hypot(x2 - x1, y2 - y1)
+
+            bright = np.interp(length, [15, 220], [0, 100])
+            print(bright, length)
+            sbc.set_brightness(int(bright))
+
+            # Hand range 15 - 220
+            # Brightness range 0 - 100
+
+        cv2.imshow('Image', img)
+        if cv2.waitKey(1) & 0xff == ord('q'):
+            break
+
+
+
+app.run(debug=True)
